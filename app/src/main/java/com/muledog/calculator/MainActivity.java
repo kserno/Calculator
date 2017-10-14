6e10bd971db516ef4914bb5f954b932066bada82
Muledog17 <maxr2025@gmail.com>
2017-10-13 23:24:49 -0400

    UI
    Created UI

--- app/src/main/java/com/muledog/calculator/MainActivity.java
+++ app/src/main/java/com/muledog/calculator/MainActivity.java
@@ -1,15 +1,21 @@
 package com.muledog.calculator;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
-
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState)
+    public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
